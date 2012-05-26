7ecf5378a6d2847e270429bc3b6986e04707f603
Dan Sterrett <dansterrett@gmail.com>
2012-05-26 18:00:19 -0400

    Update to use the latest Pushpop files.

--- vendor/assets/javascripts/pushpop_rails.js
+++ vendor/assets/javascripts/pushpop_rails.js
@@ -1,4 +1,4 @@
 //= require externals/scrollkit/scrollkit
 //= require pushpop
-//= require pushpop-tableview/pushpop-tableview
+//= require pushpop-table-view2/pushpop-table-view
 //= require pushpop-navigationbar/pushpop-navigationbar
